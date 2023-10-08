a081bebf5f13a2b36603cdbf0260d5f301779d65
Nicolas Sierra <nicolas95u@gmail.com>
2023-10-07 21:09:35 -0300

    ajustes menores

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
- let pin = prompt("Bienvenido, ingrese su pin");
- alert("Pin ingresado correctamente" + "!");
+let pin = prompt("Bienvenido, ingrese su pin");
+alert("Pin ingresado correctamente" + "!");
 
-let texto = prompt ("Indique divisa a retirar");
-let texto2 = prompt ("Caja de ahorros o Cuenta corriente ");
-
-
-let saldo = 10000;
+let saldoCajaAhorros = 10000;
+let saldoCuentaCorriente = 20000;
 let continuar = true;
 
 do {
@@ -13,38 +10,33 @@ do {
 
     switch (opcion) {
         case "1":
-            verificarSaldo();
-            function verificarSaldo() {
-                alert(`Su saldo actual es: $${saldo}`);
+            let seleccionCuenta = prompt("Seleccione una cuenta:\n1. Caja de Ahorros\n2. Cuenta Corriente");
+            if (seleccionCuenta === "1") {
+                verificarSaldo(saldoCajaAhorros);
+            } else if (seleccionCuenta === "2") {
+                verificarSaldo(saldoCuentaCorriente);
+            } else {
+                alert("Opción no válida. Por favor, elija una opción válida.");
             }
-            
             break;
         case "2":
-            retirarDinero();
-            function retirarDinero() {
-                const cantidadRetiro = parseFloat(prompt("Ingrese la cantidad a retirar:"));
-            
-                if (isNaN(cantidadRetiro) || cantidadRetiro <= 0) {
-                    alert("Por favor, ingrese una cantidad válida.");
-                } else if (cantidadRetiro > saldo) {
-                    alert("Saldo insuficiente.");
-                } else {
-                    saldo -= cantidadRetiro;
-                    alert(`Has retirado $${cantidadRetiro}. Nuevo saldo: $${saldo}`);
-                }
+            let seleccionCuentaRetiro = prompt("Seleccione una cuenta:\n1. Caja de Ahorros\n2. Cuenta Corriente");
+            if (seleccionCuentaRetiro === "1") {
+                retirarDinero(saldoCajaAhorros);
+            } else if (seleccionCuentaRetiro === "2") {
+                retirarDinero(saldoCuentaCorriente);
+            } else {
+                alert("Opción no válida. Por favor, elija una opción válida.");
             }
             break;
         case "3":
-            depositarDinero();
-            function depositarDinero() {
-                const cantidadDeposito = parseFloat(prompt("Ingrese la cantidad a depositar:"));
-            
-                if (isNaN(cantidadDeposito) || cantidadDeposito <= 0) {
-                    alert("Por favor, ingrese una cantidad válida.");
-                } else {
-                    saldo += cantidadDeposito;
-                    alert(`Has depositado $${cantidadDeposito}. Nuevo saldo: $${saldo}`);
-                }
+            let seleccionCuentaDeposito = prompt("Seleccione una cuenta:\n1. Caja de Ahorros\n2. Cuenta Corriente");
+            if (seleccionCuentaDeposito === "1") {
+                depositarDinero(saldoCajaAhorros);
+            } else if (seleccionCuentaDeposito === "2") {
+                depositarDinero(saldoCuentaCorriente);
+            } else {
+                alert("Opción no válida. Por favor, elija una opción válida.");
             }
             break;
         case "4":
@@ -56,4 +48,30 @@ do {
     }
 } while (continuar);
 
+function verificarSaldo(saldo) {
+    alert(`Su saldo actual es: $${saldo}`);
+}
 
+function retirarDinero(saldo) {
+    const cantidadRetiro = parseFloat(prompt("Ingrese la cantidad a retirar:"));
+
+    if (isNaN(cantidadRetiro) || cantidadRetiro <= 0) {
+        alert("Por favor, ingrese una cantidad válida.");
+    } else if (cantidadRetiro > saldo) {
+        alert("Saldo insuficiente.");
+    } else {
+        saldo -= cantidadRetiro;
+        alert(`Has retirado $${cantidadRetiro}. Nuevo saldo: $${saldo}`);
+    }
+}
+
+function depositarDinero(saldo) {
+    const cantidadDeposito = parseFloat(prompt("Ingrese la cantidad a depositar:"));
+
+    if (isNaN(cantidadDeposito) || cantidadDeposito <= 0) {
+        alert("Por favor, ingrese una cantidad válida.");
+    } else {
+        saldo += cantidadDeposito;
+        alert(`Has depositado $${cantidadDeposito}. Nuevo saldo: $${saldo}`);
+    }
+}
